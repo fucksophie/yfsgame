2e6026e3b556577c4f176e3a17cd5b65259bb4a5
yourfriend <friend@yourfriend.lv>
2022-03-29 20:29:51 +0300

    comit.

---
+++ backend/index.ts
@@ -0,0 +1,207 @@
+// deno-lint-ignore-file no-explicit-any
+import { serve } from "https://deno.land/std@0.132.0/http/mod.ts";
+import { Collection } from "https://deno.land/x/harmony@v2.6.0/src/utils/collection.ts";
+
+interface Location {
+  x: number;
+  y: number;
+}
+
+class User {
+  id: string;
+  name: string;
+  location!: Location;
+  ws!: WebSocket;
+  drawing!: boolean;
+
+
+  constructor(name: string, location: { x: number; y: number }, ws: WebSocket) {
+    this.name = name;
+    this.location = location;
+    this.ws = ws;
+    this.id = crypto.randomUUID();
+  }
+
+  sendPacket(type: string, packet: any) {
+    this.ws.send(JSON.stringify({
+      type,
+      ...packet,
+    }));
+  }
+}
+const square = "15x15".split('x').map(e=>+e);
+
+const map: (string | undefined)[][] = " ".repeat(square[0]).split(" ").map((_e) =>
+  " ".repeat(square[1]).split(" ")
+);
+const users: Collection<string, User> = new Collection<string, User>();
+
+const sendPacketToAll = (type: string, packet: any) =>
+  users.forEach((e) => e.sendPacket(type, packet));
+
+
+const findAvailableSquare = (): Location => {
+  let lol;
+
+  map.forEach((y, yi) => {
+    y.forEach((x, xi) => {
+      if (!x) lol = { x: xi, y: yi };
+    });
+  });
+  return lol || { x: square[0], y: square[1] }; // bugs with this,,, !U ghh!~H!H!H!HY!HH!H!U*hyqwfir0d-ghsdffgipsdfg-[9sdhjufgph]
+};
+
+const findRandomAvailableSquare = (): Location => {
+  const good: Location[] = [];
+
+  map.forEach((y, yi) => {
+    y.forEach((x, xi) => {
+      if(x && y) return;
+      good.push({ x: xi, y: yi });
+    });
+  });
+
+  return good[Math.floor(Math.random() * good.length)]
+}
+function reqHandler(req: Request) {
+  
+  if (req.headers.get("upgrade") != "websocket") {
+    
+    return new Response(null, { status: 501 });
+  }
+  const { socket: ws, response } = Deno.upgradeWebSocket(req);
+
+  let id: string;
+
+  ws.addEventListener("open", () => {
+    if(users.size >= square[0]*square[1]) {
+      
+      ws.send(JSON.stringify({
+        type: "warning", 
+        message: "[ERROR] Game is full!"
+      }))
+
+      return ws.close(0);
+    }
+
+    const user = new User("0", findAvailableSquare(), ws);
+
+    sendPacketToAll("join", {
+      user,
+    });
+
+    users.set(user.id, user);
+
+    id = user.id;
+
+    user.sendPacket("map", {map});
+
+    user.sendPacket("users", 
+    { users: users.map(e => {return {id: e.id, location: e.location, name: e.name}})}
+    );
+
+
+    user.sendPacket("hi", {user});
+
+
+    map[user.location.y][user.location.x] = user.id;
+  });
+  ws.addEventListener("message", message => {
+      // proper parsing here
+      
+      // handle json errors
+
+      const json = JSON.parse(message.data);
+      const user = users.get(id);
+      
+      if(!user) return ws.close();
+
+        if(json.type == "startDrawing") {
+          sendPacketToAll("startDrawing", {
+            id: user.id
+          })
+          user.drawing = true;
+          users.set(user.id, user)
+        }
+
+        if(json.type == "stopDrawing") {
+          sendPacketToAll("stopDrawing", {
+            id: user.id
+          })
+          user.drawing = false;
+          users.set(user.id, user)
+        }
+
+        if(json.type == "move") {
+            const oldpos = {...user.location}; // {...} trick so we can make new obj
+
+            if(json.location == "up") user.location.y -= 1;
+            if(json.location == "down") user.location.y += 1;
+
+            if(json.location == "left") user.location.x -= 1;
+            if(json.location == "right") user.location.x += 1;
+
+            if(user.location.x < 0 || user.location.y < 0 || user.location.y > square[0]
+                || user.location.x > square[1]) {
+                user.location = oldpos;
+                
+                return;
+            } 
+
+            if(map[user.location.y][user.location.x] == "food") {
+              map[user.location.y][user.location.x] = "";
+              if(user.name !== "100") {
+                user.name = (+user.name)+1+""
+
+              } 
+              sendPacketToAll("update", {
+                user
+              });
+            }
+
+            if(map[user.location.y][user.location.x]) {
+              user.location = oldpos;
+              return;
+            }
+            map[oldpos.y][oldpos.x] = ''            
+            map[user.location.y][user.location.x] = user.id;
+
+            users.set(user.id, user)
+
+            sendPacketToAll("move", {
+                id: user.id, 
+                location: user.location
+            });
+        }
+
+  })
+  ws.addEventListener("close", () => {
+    const user = users.get(id);
+    
+    if(!user) return;
+
+    map[user.location.y][user.location.x] = "";
+
+    users.delete(id);   
+    sendPacketToAll("bye", {
+      id: user.id
+    }) 
+    
+    
+  });
+  
+  return response;
+}
+
+serve(reqHandler, { port: 8000 });
+console.log("started")
+
+setInterval(() => {
+  if(users.size == 0) return;
+  const location = findRandomAvailableSquare();
+  if(!location) return;
+  map[location.y][location.x] = "food"
+  sendPacketToAll("food", {
+    location
+  })
+}, 2000)
---
+++ frontend/script.js
@@ -0,0 +1,176 @@
+const ws = new WebSocket(location.hostname === "localhost" || location.hostname === "127.0.0.1" ? "ws://192.168.1.125:8000" : "wss://api-game.yourfriend.lv");
+let users = [];
+let user = "";
+
+function drawTile(id, location) {
+    const ussr = users.find(e => e.id == id);
+    const tile = document.getElementById(`x${location.x}y${location.y}`);
+
+    if (user == id) {
+        tile.style.backgroundColor = "red"
+        tile.style.color = "white"
+    }
+
+    tile.innerText = ussr.name;
+}
+
+function clearTile(location) {
+    const tile = document.getElementById(`x${location.x}y${location.y}`);
+    tile.removeAttribute('style')
+    tile.innerText = "";
+}
+
+ws.addEventListener("open", () => {
+    console.log('cnn')
+})
+
+document.addEventListener("keydown", event => {
+    if (event.which == '40') {
+        ws.send(JSON.stringify({
+            type: "move",
+            location: "down"
+        }))
+    }
+    if (event.which == '38') {
+        ws.send(JSON.stringify({
+            type: "move",
+            location: "up"
+        }))
+    }
+    if (event.which == '37') {
+        ws.send(JSON.stringify({
+            type: "move",
+            location: "left"
+        }))
+    }
+    if (event.which == '39') {
+        ws.send(JSON.stringify({
+            type: "move",
+            location: "right"
+        }))
+    }
+    if(event.which == '82') {
+        const ussr = users.find(e => e.id == user);
+
+        if(ussr.drawing) {
+            ws.send(JSON.stringify({
+                type: "stopDrawing",
+            }))
+            return;
+        }
+        ws.send(JSON.stringify({
+            type: "startDrawing",
+        }))
+    }
+})
+
+ws.addEventListener("message", msg => {
+    const json = JSON.parse(msg.data);
+    console.log(json)
+    if(json.type == "warning") {
+        alert(json.message)
+    }
+    if (json.type == "map") {
+        json.map.forEach((y, yi) => {
+            const tr = document.createElement("tr")
+    
+            y.forEach((x, xi) => {
+                const td = document.createElement("td");
+                if(x == "food") {
+                    td.style.backgroundColor = "yellow"
+                    td.innerText = "food"
+                } 
+                td.id = `x${xi}y${yi}`
+                
+                tr.appendChild(td);
+            });
+    
+            document.getElementById("map").appendChild(tr);
+        });
+    }
+
+    if (json.type == "join") {
+        users.push(json.user);
+
+        drawTile(json.user.id, json.user.location);
+    }
+
+    if(json.type == "startDrawing") {
+        const ussr = users.find(e => e.id == json.id);
+
+        ussr.drawing = true;
+    }
+    if(json.type == "stopDrawing") {
+        const ussr = users.find(e => e.id == json.id);
+
+        ussr.drawing = false;
+    }
+    if(json.type == "food") {
+        const tile = document.getElementById(`x${json.location.x}y${json.location.y}`);
+        
+        tile.style.backgroundColor = "yellow"
+        tile.innerText = "i am food."
+    }
+    if (json.type == "move") {
+        const ussr = users.find(e => e.id == json.id);
+        
+        clearTile(ussr.location)
+
+        if(ussr.drawing) {
+            const tile = document.getElementById(`x${ussr.location.x}y${ussr.location.y}`);
+            if(ussr.name == "100") {
+                tile.style.backgroundColor = "red"
+            } else if(+ussr.name >= 80) {
+                tile.style.backgroundColor = "green"
+            } else if(+ussr.name >= 60) {
+                tile.style.backgroundColor = "blue"
+            }  else if(+ussr.name >= 30) {
+                tile.style.backgroundColor = "aqua"
+            } else if(+ussr.name >= 10) {
+                tile.style.backgroundColor = "pink"
+            } else {
+                tile.style.backgroundColor = "grey"
+            }
+
+            const loc = {...ussr.location}
+            setTimeout(() => {
+                clearTile(loc)
+                const u55r = users.find(e => e.id == user);
+
+                drawTile(u55r.id, u55r.location)
+
+            }, (+ussr.name)*100)
+        }
+
+        ussr.location = json.location;
+        clearTile(ussr.location)
+
+        drawTile(json.id, json.location);
+
+    }
+    
+    if(json.type == "update") {
+        const ussr = users.find(e => e.id == json.user.id);
+        ussr.name = json.user.name;
+    }
+
+    if (json.type == "bye") {
+        clearTile(users.find(e => e.id == id).location);
+        users = users.filter(e => e.id != json.id);
+    }
+
+    if (json.type == "users") {
+        users = json.users;
+
+        users.forEach(u => {
+            drawTile(u.id, u.location)
+        })
+    }
+    
+    if (json.type == "hi") {
+        user = json.user.id;
+
+        drawTile(json.user.id, json.user.location)
+    }
+
+})
